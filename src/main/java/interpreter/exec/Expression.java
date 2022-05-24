06651f7d4a3d23e6b0eab0ac5babd2080eb0699f
Grebenshchikova, Natalia (Ext) <natalia.grebenshchikova_ext@novartis.com>
2022-05-24 13:01:44 +0300

    iterator  behavior pattern

---
+++ src/main/java/interpreter/exec/Expression.java
@@ -0,0 +1,9 @@
+package interpreter.exec;
+
+/*
+ * Created by grebena1 on 24.05.2022
+ * Description:
+ */
+public interface Expression {
+    String interpret(String context);
+}
---
+++ src/main/java/itterator/exec/Employee.java
@@ -0,0 +1,24 @@
+package itterator.exec;
+
+/*
+ * Created by grebena1 on 24.05.2022
+ * Description:
+ */
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}

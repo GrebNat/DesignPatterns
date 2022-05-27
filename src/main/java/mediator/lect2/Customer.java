d2f95836b211ad4d636089931edd417dd583567d
Grebenshchikova, Natalia (Ext) <natalia.grebenshchikova_ext@novartis.com>
2022-05-27 15:39:32 +0300

    mediator behavior pattern

---
+++ src/main/java/mediator/lect2/Customer.java
@@ -0,0 +1,18 @@
+package mediator.lect2;
+
+/*
+ * Created by grebena1 on 24.05.2022
+ * Description:
+ */
+public class Customer {
+    private String address;
+
+    public Customer(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+}
